b512d3d463237f814788c3db8bea81f2c48304b4
Michael Wells <mwells@sygnal.com>
2019-03-19 00:06:21 +1300

    Updated

--- src/app/enums1/enums1.module.ts
+++ src/app/enums1/enums1.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, LowerCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
--- src/app/enums1/enums1.page.ts
+++ src/app/enums1/enums1.page.ts
@@ -6,27 +6,27 @@ import { Router } from '@angular/router';
 //import { EnumKeysPipe } from '../enum-keys.pipe';
 
 export enum FooEnum {
-  Option_1,
-  Option_2,
-  Option_3,
-  Option_4,
-  Option_5,
+  Option_1 = 1,
+  Option_2 = 2,
+  Option_3 = 3,
+  Option_4 = 4,
+  Option_5 = 5,
 }
 
-export namespace FooEnum {
+// export namespace FooEnum {
 
-  // Retrieves the string name of a specific enum value
-  // We convert underscores to spaces for display
-  export function getName(e: FooEnum): string {
-      return FooEnum[e].toString().replace('_', ' ');
-  }
+//   // Retrieves the string name of a specific enum value
+//   // We convert underscores to spaces for display
+//   export function getName(e: FooEnum): string {
+//       return FooEnum[e].toString().replace('_', ' ');
+//   }
 
-  // Retrieves the set of all Keys
-  export function keys() {
-      return Object.keys(FooEnum);
-  }
+//   // Retrieves the set of all Keys
+//   export function keys() {
+//       return Object.keys(FooEnum);
+//   }
 
-}
+// }
 
 
 
@@ -38,9 +38,14 @@ export namespace FooEnum {
 })
 export class Enums1Page implements OnInit {
 
-  //foo = 'Option_5'; // FooEnum.Option_5;
+  public dataVal; // = FooEnum.Option_4;
 
-  dataVal: FooEnum;
+  get getFooEnum(): string {
+    return this.dataVal.toString();
+  }
+  set getFooEnum(u: string) {
+    this.dataVal = +u;
+  }
 
   // for access to the Enums, within this context,
   // needed for template access?
@@ -48,8 +53,8 @@ export class Enums1Page implements OnInit {
 
   constructor(
     private router: Router,
-//    private enumKeys: EnumKeysPipe
   ) {
+    this.initializeData();
   }
 
   ngOnInit() {
@@ -76,7 +81,7 @@ export class Enums1Page implements OnInit {
   initializeData() {
 
     // Set any initial values
-    this.dataVal = FooEnum.Option_5;
+    this.dataVal = FooEnum.Option_4;
 
   }
 
